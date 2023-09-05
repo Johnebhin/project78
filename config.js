8b3878c4eac51c6df40d9b77562eafb4bf885f4c
Johnebhin <121502677+Johnebhin@users.noreply.github.com>
2023-09-05 20:51:16 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+  const firebaseConfig = {
+    apiKey: "AIzaSyCIXqocHFIMPuaMbbb7FYfb6zP56Nh6IR4",
+    authDomain: "e-ride-c6c44.firebaseapp.com",
+    projectId: "e-ride-c6c44",
+    storageBucket: "e-ride-c6c44.appspot.com",
+    messagingSenderId: "589856094235",
+    appId: "1:589856094235:web:a87671151baf5117d760be"
+  };
